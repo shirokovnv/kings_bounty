82c54057aaa90c3192cc2c9dc94a4f4a7510cf8d
shirokovnv <shirokovnv@gmail.com>
2024-06-16 20:07:52 +0500

    Add little X-axis offset for camera controller

--- Assets/Scripts/Adventure/Controllers/CameraController.cs
+++ Assets/Scripts/Adventure/Controllers/CameraController.cs
@@ -4,12 +4,14 @@ namespace Assets.Scripts.Adventure.Controllers
 {
     public class CameraController : MonoBehaviour
     {
+        private static float offsetX = 0.5f;
+
         [SerializeField] private Transform target;
 
         void LateUpdate()
         {
             transform.position = new Vector3(
-                target.transform.position.x,
+                target.transform.position.x + offsetX,
                 target.transform.position.y,
                 transform.position.z
                 );
